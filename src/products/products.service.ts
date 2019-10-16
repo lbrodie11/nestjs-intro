84d66b55325b8acf9eb1e3e4ce098d8b56a0cbce
Laqwan Brodie <lbrodie@gmail.com>
2019-10-15 21:53:40 -0400

    created a basic crud app

---
+++ src/products/products.service.ts
@@ -0,0 +1,53 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+
+import { Product } from './product.model'
+
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [];
+
+  insertProduct(title: string, desc: string, price: number) {
+    const prodId = Math.random().toString();
+    const newProduct = new Product(prodId, title, desc, price )
+    this.products.push(newProduct)
+    return prodId
+  }
+
+  getProducts(){
+    return [...this.products];
+  }
+
+  getSingleProduct(prodId: string){
+    const product =  this.findProduct(prodId)[0];
+    return {...product};
+  }
+
+  updateProduct(prodId: string, title: string, desc: string, price: number){
+    const [product, index] = this.findProduct(prodId)
+    const updatedProduct = {...product}
+    if(title){
+      updatedProduct.title = title
+    } 
+    if(desc){
+      updatedProduct.description = desc
+    }
+    if(price){
+      updatedProduct.price = price
+    }
+    this.products[index] = updatedProduct;
+  }
+
+  deleteProduct(prodId: string){
+    const [product, index] = this.findProduct(prodId);
+    this.products.splice(index, 1);
+  }
+
+  private findProduct(prodId: string): [Product, number]{
+    const productIndex = this.products.findIndex((prod) => prod.id === prodId);
+    const product = this.products[productIndex]
+    if(!product){
+      throw new NotFoundException('Could not find Product')
+    }
+    return [product, productIndex]
+  }
+}
